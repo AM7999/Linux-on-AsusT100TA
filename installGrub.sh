4fc3551886c33dd4aafbfbe5febf5110c46e1884
AM7999 <AM799912122019@gmail.com>
2024-03-25 16:12:55 +0000

    push some html and other things

--- installGrub.sh
+++ installGrub.sh
@@ -1,6 +1,5 @@
-!#bin/sh
-
+#!bin/sh
 echo "Installing Grub.."
-grub install --target=i386-efi --efi-directory=/boot/EFI --removable
+grub install --target=i386-efi --efi-directory=/boot/EFI --removable > /dev/null
 echo "Rebooting"
-shutdown -r
+shutdown -r > /dev/null
